1656da90dd87dbabe3197820796d2515b37c4cd1
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-02-11 00:08:11 -0300

    modify http response.badRequest to receive an error

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -193,4 +193,20 @@ describe('Login Router', () => {
 
         expect(httpResponse.statusCode).toBe(500)
     })
+
+    /* test('Should return 400 if email provided is invalid', async () => {
+        const { sut } = makeSut()
+
+        const httpRequest = {
+            body: {
+                email: 'invalid_email@gmail.com',
+                password: 'any_password'
+            }
+        }
+
+        const httpResponse = await sut.route(httpRequest)
+
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new InvalidParamError('email'))
+    }) */
 })
