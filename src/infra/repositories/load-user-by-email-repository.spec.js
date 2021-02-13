af48c13a7c711a0a6e2ae88fb5ede635feb99031
José Lucas Gaspar <joselucas.gaspar@gmail.com>
2021-02-13 17:37:29 -0300

    ensure LoadUserByEmailRepository returns an user id with id and password only

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -6,7 +6,11 @@ class LoadUserByEmailRepository {
     }
 
     async load(email) {
-        const user = await this.userModel.findOne({ email: email })
+        const user = await this.userModel.findOne({ email: email }, {
+            projection: {
+                password: 1 // Quero id e password, mas o id já vem por padrão
+            }
+        })
 
         return user
     }
@@ -23,7 +27,6 @@ const makeSut = () => {
 }
 
 describe('LoadUserByEmail Repository', () => {
-    
     beforeAll(async () => {
         client = await MongoClient.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
@@ -50,12 +53,19 @@ describe('LoadUserByEmail Repository', () => {
     test('Should return an user if user is found', async () => {
         const { sut, userModel } = makeSut()
 
-        await userModel.insertOne({ email: 'valid_email@mail.com' })
+        const fakeUser = await userModel.insertOne({
+            email: 'valid_email@mail.com',
+            name: 'any_name',
+            age: 50,
+            state: 'any_state',
+            password: 'hashed_password'
+        })
 
         const user = await sut.load('valid_email@mail.com')
 
-        expect(user.email).toBe('valid_email@mail.com')
+        expect(user).toEqual({
+            _id: fakeUser.ops[0]._id,
+            password: fakeUser.ops[0].password
+        })
     })
-
-
 })
